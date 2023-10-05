8df205d6486636df325ef120ab8deee2260029a2
Amianate <Alexwild737@gmail.com>
2023-10-05 17:10:05 -0400

    Regis pw validation

--- tpl/regisValidation.php
+++ tpl/regisValidation.php
@@ -17,13 +17,37 @@ class regisValidation
     {
         $capFlag = false;
         $numFlag = false;
-        $length = strlen($pw);
+        $length = strlen($this->pw);
+
+        //Checking pw length
+        if($length < 10){
+            throw new \Exception("The password is not long enough");
+        }
+
+        //Checking for caps 
+        for($x = 0; $x < $length; $x++){
+            if( ctype_upper($this->pw[$x]) ){
+                $capFlag = true;
+            }
+        } 
+
+        // Checking for numbers
+        for($x = 0; $x < $length; $x++){
+            if( ctype_digit($this->pw[$x]) ){
+                $numFlag = true;
+            }
+        } 
+
+        if($capFlag == true && $numFlag == true){
+            //Do something with the database
+            echo password_hash($this->pw, PASSWORD_DEFAULT);
+        }
     }
 
-    function returnError($string)
-    {
-        throw new \Exception($string);
-    }
+    // function returnError($string)
+    // {
+    //     throw new \Exception($string);
+    // }
 }
 
 
